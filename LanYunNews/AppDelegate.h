11a4e4c47cdc64aef783b25ba8e4c4d989f4925d
wanghanfeng <285360418@qq.com>
2017-01-11 20:22:12 +0800

    Initial Commit

---
+++ LanYunNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LanYunNews
+//
+//  Created by whf on 17/1/11.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LanYunNews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LanYunNews
+//
+//  Created by whf on 17/1/11.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
