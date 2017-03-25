51894b9d3cbe3626a7b5bebb3802d30d2e26711b
wanghanfeng <285360418@qq.com>
2017-03-25 16:39:02 +0800

    ui changed

--- LanYunNews/MenuV.m
+++ LanYunNews/MenuV.m
@@ -11,11 +11,33 @@
 @implementation MenuV
 
 - (instancetype)initWithFrame:(CGRect)frame {
-    
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor colorWithWhite:.65f alpha:.3f];
+        [self addMenu];
+    }
+    return self;
 }
 
 - (void)addMenu{
     UIView *menuV = [[UIView alloc] initWithFrame:CGRectMake(10, 0, self.bounds.size.width-20, 67)];
+    menuV.backgroundColor = [UIColor whiteColor];
+    UIButton *btn1 = [UIButton buttonWithType:UIButtonTypeSystem];
+    btn1.tag = 1;
+    [btn1 setTitle:@"点我" forState:UIControlStateNormal];
+    [btn1 setTitle:@"你点到了" forState:UIControlStateSelected];
+    btn1.frame = CGRectMake(5, 10, 40, 40);
+    [btn1 addTarget:self action:@selector(btnPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(btnPressed:)];
+    [self addGestureRecognizer:tapGestureRecognizer];
+}
+
+- (void)btnPressed:(id)sender {
+    UIButton *btn = [self viewWithTag:1];
+    if (btn == sender) {
+        NSLog(@"%@",btn.titleLabel.text);
+    }
 }
 
 @end
